ddd1520a785e58f7179fde69c66973e6969927f1
monika2416090 <mithunm.it2023@citchennai.net>
2025-04-02 19:02:40 +0530

    commit

--- backend/models/Event.js
+++ backend/models/Event.js
@@ -5,6 +5,7 @@ const eventSchema = new mongoose.Schema({
   eventName: { type: String, required: true },
   place: { type: String, required: true },
   time: { type: String, required: true },
+  date: {type: Date, required:true},
 });
 
 module.exports = mongoose.model('Event', eventSchema);
