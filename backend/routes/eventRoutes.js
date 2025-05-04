03facd42d2fbcc313195bc6dc837c91a5d811ca8
monika2416090 <mithunm.it2023@citchennai.net>
2025-05-04 18:51:56 +0530

    modif

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 // Create event requires JWT token for authentication
 router.post('/create-event', createEvent);
 router.get('/get-events', getEvents); 
-router.get('/event/:companyName/:eventName', getEventByDetails);
+router.get('/:companyName/:eventName', getEventByDetails);
 
 
 module.exports = router;
