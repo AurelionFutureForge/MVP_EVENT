765fc606138b125db4c90847b316dabe663b80b8
monika2416090 <mithunm.it2023@citchennai.net>
2025-05-13 18:15:05 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 
 // ✅ Serve static files from the 'uploads' folder
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "src", "uploads")));
 
 // CORS config
 const corsOptions = {
