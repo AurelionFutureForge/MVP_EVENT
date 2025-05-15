8e6db5df393e1aa8339bcde1ce87259b305ac60d
monika2416090 <mithunm.it2023@citchennai.net>
2025-05-16 01:38:39 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use('/uploads', (req, res, next) => {
 
 
 const corsOptions = {
-  origin: ["https://mvp-event.vercel.app", "http://localhost:5173"],  
+  origin: ["https://mvp-event.vercel.app", "http://localhost:5173","https://events.aurelionfutureforge.com"],  
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: ["Content-Type", "Authorization"]
 };
