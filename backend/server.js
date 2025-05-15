4f6b5d160d46e120e51aaef14eb0bef26d0ebc4d
monika2416090 <mithunm.it2023@citchennai.net>
2025-05-15 20:04:32 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -16,25 +16,12 @@ const phonepeRoutes = require('./routes/phonepeRoutes')
 const app = express();
 app.use(express.json());
 
-app.use('/uploads', express.static(path.resolve('/opt/render/project/src/uploads'), {
-  setHeaders: (res, filePath) => {
-    // Set proper CORS headers
-    res.setHeader('Access-Control-Allow-Origin', '*');
+aapp.use('/uploads', (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  next();
+}, express.static(path.resolve('/opt/render/project/src/uploads')));
 
-    // Optional but useful: Ensure correct MIME type (especially helpful with older or misconfigured setups)
-    if (filePath.endsWith('.jpg')) {
-      res.setHeader('Content-Type', 'image/jpeg');
-    } else if (filePath.endsWith('.png')) {
-      res.setHeader('Content-Type', 'image/png');
-    } else if (filePath.endsWith('.jpeg')) {
-      res.setHeader('Content-Type', 'image/jpeg');
-    } else if (filePath.endsWith('.webp')) {
-      res.setHeader('Content-Type', 'image/webp');
-    }
-  }
-}));
 
-// CORS config
 const corsOptions = {
   origin: ["https://mvp-event.vercel.app", "http://localhost:5173"],  
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
